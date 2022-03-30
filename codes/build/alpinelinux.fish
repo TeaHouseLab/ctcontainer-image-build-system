f99e4108cd2df17272d3435ab23a2e94ee14ff39
tsingkwai <tsingkwai@bluelaketable>
2022-03-30 09:46:33 +0800

    Remove aarch64 for alpinelinux

--- codes/build/alpinelinux.fish
+++ codes/build/alpinelinux.fish
@@ -4,8 +4,10 @@ function alpinelinux
     else
         mkdir alpinelinux
     end
-    env http_proxy=http://127.0.0.1:7890 https_proxy=http://127.0.0.1:7890 apk --arch x86_64 -X https://dl-cdn.alpinelinux.org/alpine/edge/main/ -U --allow-untrusted --root /ctcontainer_share/alpinelinux --initdb add alpine-base bash fish
+    env apk --arch x86_64 -X https://dl-cdn.alpinelinux.org/alpine/edge/main/ -U --allow-untrusted --root /ctcontainer_share/alpinelinux --initdb add alpine-base bash fish coreutils
     cd alpinelinux
+    echo "https://dl-cdn.alpinelinux.org/alpine/edge/main
+http://dl-cdn.alpinelinux.org/alpine/edge/community" >etc/apk/repositories
     tar zcf alpinelinux.tar.gz .
     cd ..
     mv alpinelinux/alpinelinux.tar.gz .
