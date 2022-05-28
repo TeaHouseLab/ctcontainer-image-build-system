df59f7e849c4f36a6b59738ea1d6a7d85c81a85a
happyeggchen <tsingkwai@protonmail.com>
2022-05-28 11:52:38 +0800

    fixed something

--- codes/build/alpinelinux.fish
+++ codes/build/alpinelinux.fish
@@ -1,12 +1,12 @@
 function alpinelinux
-    logger "Building alpinelinux edge amd64"
+    logger 3 "Building alpinelinux edge amd64"
     if sudo distrobuilder build-lxc ./files/alpinelinux.yaml -o image.architecture=x86_64 -o image.release=edge -o image.variant=default
         rm meta.tar.xz && mv rootfs.tar.xz alpinelinux
     else
         logger 4 "error when building alpinelinux edge amd64"
     end
-    logger "Building alpinelinux edge arm64"
-    if sudo distrobuilder build-lxc alpinelinux.yaml -o image.architecture=aarch64 -o image.release=edge -o image.variant=default
+    logger 3 "Building alpinelinux edge arm64"
+    if sudo distrobuilder build-lxc ./files/alpinelinux.yaml -o image.architecture=aarch64 -o image.release=edge -o image.variant=default
         rm meta.tar.xz && mv rootfs.tar.xz alpinelinuxarm64
     else
         logger 4 "error when building alpinelinux edge arm64"
--- codes/build/archlinux.fish
+++ codes/build/archlinux.fish
@@ -1,12 +1,12 @@
 function archlinux
-    logger "Building archlinux amd64"
+    logger 3 "Building archlinux amd64"
     if sudo distrobuilder build-lxc ./files/archlinux.yaml -o image.architecture=x86_64 -o image.variant=default
         rm meta.tar.xz && mv rootfs.tar.xz archlinux
     else
         logger 4 "error when building archlinux amd64"
     end
-    logger "Building archlinux arm64"
-    if sudo distrobuilder build-lxc archlinux.yaml -o image.architecture=aarch64 -o image.variant=default
+    logger 3 "Building archlinux arm64"
+    if sudo distrobuilder build-lxc ./files/archlinux.yaml -o image.architecture=aarch64 -o image.variant=default
         rm meta.tar.xz && mv rootfs.tar.xz archlinuxarm64
     else
         logger 4 "error when building archlinux arm64"
